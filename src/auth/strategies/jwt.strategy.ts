8bdf770024986cbbd695e7e7aab8a598f9d37d81
IsraelFuertes <israfuertes6@gmail.com>
2026-06-29 13:16:56 -0400

    Corrige validación de sesión en JwtStrategy

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,18 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+import { RefreshTokensService } from '../../refresh-tokens/refresh-tokens.service';
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(
+    private readonly refreshTokensService: RefreshTokensService,
+  ) {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest:
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_ACCESS_SECRET!,
+      secretOrKey:
+        process.env.JWT_ACCESS_SECRET!,
     });
   }
 
   async validate(payload: any) {
+    const sesiones =
+      await this.refreshTokensService.findValidTokens(
+        payload.sub,
+      );
+
+    if (sesiones.length === 0) {
+      throw new UnauthorizedException(
+        'Sesión inválida',
+      );
+    }
+
     return {
       id: payload.sub,
       email: payload.email,
